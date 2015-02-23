03046d7f1e3c44694506d459aa7f81aafa6f762e
Sylvain Lobry <sylvain.lobry@telecom-paristech.fr>
2015-02-23 14:14:08 +0100

    Added functions to write an ima file.
    	  * ximaIO.py: Here.

--- ximaIO.py
+++ ximaIO.py
@@ -9,8 +9,15 @@ def imaread(imgName):
 
 	return _imaread(imgName)
 
+def imawrite(img, imgName):
+	"""Write img to imgName. imgName can come with or without extension"""
+	if imgName.endswith('.ima'):
+		imgName = os.path.splitext(imgName)[0]
+
+	return _imawrite(img, imgName)
+
 def _imaread(imgName):
-	"""Reads a *.ima file. ImgName should come with no extension"""
+	"""Reads a *.ima file. imgName should come with no extension"""
 	with open(imgName + '.dim') as f:
 		tmp = f.readline().split()
 		w = int(tmp[0])
@@ -23,3 +30,16 @@ def _imaread(imgName):
 				img[i, j] = struct.unpack('B', f.read(1))[0]
 
 	return img
+
+def _imawrite(img, imgName):
+	"""Write img to an imgName.ima file. imgName should come with no extension"""
+	w = img.shape[1]
+	h = img.shape[0]
+	with open(imgName + '.dim', 'w') as f:
+		f.write(str(w) + ' ' + str(h))
+
+	with open(imgName + '.ima', 'w') as f:
+		for i in range(0, h):
+			for j in range(0, w):
+				data = struct.pack('B', img[i, j])
+				f.write(data)
